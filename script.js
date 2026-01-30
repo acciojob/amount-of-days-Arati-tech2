44e97627d7944be8046595cde8e1b6659babf39c
Arati-tech2 <aratiwak2@gmail.com>
2026-01-30 22:57:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-//your JS code here. If required.
-function daysOfAYear(year){
-	if((year% 400 == 0) || (year % 4 ==0 && year % 100 != 0)){
-		return 366;
-	}
-	else{
-		retrun 365;
-	}
+function daysOfAYear(year) {
+  // Leap year check
+  if ((year % 400 === 0) || (year % 4 === 0 && year % 100 !== 0)) {
+    return 366;
+  }
+  return 365;
 }
